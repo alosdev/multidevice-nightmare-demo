c518ab652559092887656af2b79a3d4e070cc6dd
alosdev <hasanhosgel@googlemail.com>
2013-06-07 14:30:38 +0200

    use boolean flags for device classifications

--- MultideviceNightmareDemo/src/main/java/de/alosdev/demo/multidevicenichtmare/MainActivity.java
+++ MultideviceNightmareDemo/src/main/java/de/alosdev/demo/multidevicenichtmare/MainActivity.java
@@ -2,6 +2,7 @@ package de.alosdev.demo.multidevicenichtmare;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        final String toastText;
+        if (getResources().getBoolean(R.bool.is_phone_other))
+            toastText = "phone other";
+        else if (getResources().getBoolean(R.bool.is_phone_small))
+            toastText = "phone small";
+        else if (getResources().getBoolean(R.bool.is_tablet_10))
+            toastText = "tabelt 10 inch";
+        else if (getResources().getBoolean(R.bool.is_tablet_7))
+            toastText = "tablet 7 inch";
+        else
+            toastText = "undefined";
+        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
     }
 }
