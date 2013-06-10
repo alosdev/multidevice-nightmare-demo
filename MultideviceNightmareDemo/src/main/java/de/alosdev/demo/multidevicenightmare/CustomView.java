078cee1c0bad6c07d0326484ce2a7dfa1551d828
alosdev <hasanhosgel@googlemail.com>
2013-06-10 14:37:58 +0200

    add custom attributes

--- MultideviceNightmareDemo/src/main/java/de/alosdev/demo/multidevicenightmare/CustomView.java
+++ MultideviceNightmareDemo/src/main/java/de/alosdev/demo/multidevicenightmare/CustomView.java
@@ -2,6 +2,7 @@ package de.alosdev.demo.multidevicenightmare;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,6 +16,8 @@ import static android.widget.LinearLayout.LayoutParams.WRAP_CONTENT;
  * Created by hhosgel on 10.06.13.
  */
 public class CustomView extends LinearLayout {
+    private static final int[] ORIENTATION = new int[]{HORIZONTAL, VERTICAL};
+
     public CustomView(Context context) {
         super(context);
         init(context, null, 0);
@@ -34,17 +37,36 @@ public class CustomView extends LinearLayout {
     private void init(Context context, AttributeSet attrs, int defStyle) {
         int margin = (int) getResources().getDimension(R.dimen.custom_view_margin);
         setPadding(margin, margin, margin, margin);
+
+        LayoutParams lp = new LayoutParams(MATCH_PARENT, WRAP_CONTENT, 1f);
+        lp.setMargins(margin, margin, margin, margin);
+        if (null == attrs){
+            initWithoutAttribute(context, lp, margin);
+        } else
+        initWithAttributes(context, attrs, lp, margin);
+
+        setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.custom_view_bg1)));
+    }
+
+    private void initWithAttributes(Context context, AttributeSet attrs, LayoutParams lp, int margin) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CustomView);
+        try {
+            setOrientation(ORIENTATION[a.getInt(R.styleable.CustomView_orientation, 0)]);
+            addView(createTextView(context, a.getString(R.styleable.CustomView_label), margin), lp);
+            addView(createTextView(context, a.getString(R.styleable.CustomView_value), margin), lp);
+        } finally {
+            a.recycle();
+        }
+    }
+
+    private void initWithoutAttribute(Context context, LayoutParams lp, int margin) {
         if (getResources().getBoolean(R.bool.is_phone_other) || getResources().getBoolean(R.bool.is_phone_small)) {
             setOrientation(VERTICAL);
         } else {
             setOrientation(HORIZONTAL);
         }
-        LayoutParams lp = new LayoutParams(MATCH_PARENT, WRAP_CONTENT, 1f);
-
-        lp.setMargins(margin, margin, margin, margin);
         addView(createTextView(context, "label", margin), lp);
         addView(createTextView(context, "desc", margin), lp);
-        setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.custom_view_bg1)));
     }
 
     private View createTextView(Context context, String label, int padding) {
